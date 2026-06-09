eafba4d495f01ce4d70e5d2d6c4cf14feee6e882
Christopher Schuchardt <cschuchardt88@gmail.com>
2026-06-08 23:01:44 -0400

    Added wallet models and tests Added json converters for wallet address and hex formatting and tests Added `UInt160` to wallet address extensions and back to `UInt160` and tests Added `WalletBase` class and `GetKeyFromWifString` method and tests Moved `InternalsVisibleTo` from project files to `Directory.Build.props` file

--- tests/Neo.Configuration.Tests/Model/UT_ProtocolSettingsModel.cs
+++ tests/Neo.Configuration.Tests/Model/UT_ProtocolSettingsModel.cs
@@ -29,7 +29,7 @@ namespace Neo.Configuration.Tests.Model
     public class UT_ProtocolSettingsModel
     {
         [TestMethod]
-        public void CheckPropertyValues()
+        public void TestPropertyValues()
         {
             var jsonTestString = "{\"network\":810960196,\"addressVersion\":53,\"millisecondsPerBlock\":1000,\"maxTransactionsPerBlock\":512,\"memoryPoolMaxTransactions\":50000,\"maxTraceableBlocks\":2102400,\"hardforks\":{\"HF_Aspidochelone\":666},\"initialGasDistribution\":5200000000000000,\"validatorsCount\":1,\"standbyCommittee\":[\"03b209fd4f53a7170ea4444e0cb0a6bb6a53c2bd016926989cf85f9b0fba17a70c\"],\"seedList\":[\"seed1.neo.org:10333\"]}";
             var actualProtocolOptionsModel = JsonModel.FromJson<ProtocolSettingsModel>(jsonTestString, TestDefaults.JsonDefaultSerializerOptions);
@@ -53,6 +53,35 @@ namespace Neo.Configuration.Tests.Model
 
             Assert.IsNotNull(actualProtocolOptionsModel.SeedList);
             Assert.AreEqual("seed1.neo.org:10333", actualProtocolOptionsModel.SeedList[0]);
+        }
+
+        [TestMethod]
+        public void TestObjectToProtocolSettings()
+        {
+            var jsonTestString = "{\"network\":810960196,\"addressVersion\":53,\"millisecondsPerBlock\":1000,\"maxTransactionsPerBlock\":512,\"memoryPoolMaxTransactions\":50000,\"maxTraceableBlocks\":2102400,\"hardforks\":{\"HF_Aspidochelone\":666},\"initialGasDistribution\":5200000000000000,\"validatorsCount\":1,\"standbyCommittee\":[\"03b209fd4f53a7170ea4444e0cb0a6bb6a53c2bd016926989cf85f9b0fba17a70c\"],\"seedList\":[\"seed1.neo.org:10333\"]}";
+            var actualProtocolOptionsModel = JsonModel.FromJson<ProtocolSettingsModel>(jsonTestString, TestDefaults.JsonDefaultSerializerOptions);
+
+            Assert.IsNotNull(actualProtocolOptionsModel);
+
+            var actualProtocolSettings = actualProtocolOptionsModel.ToObject();
+
+            Assert.AreEqual(810960196u, actualProtocolSettings.Network);
+            Assert.AreEqual(53, actualProtocolSettings.AddressVersion);
+            Assert.AreEqual(1_000u, actualProtocolSettings.MillisecondsPerBlock);
+            Assert.AreEqual(512u, actualProtocolSettings.MaxTransactionsPerBlock);
+            Assert.AreEqual(50_000, actualProtocolSettings.MemoryPoolMaxTransactions);
+            Assert.AreEqual(2_102_400u, actualProtocolSettings.MaxTraceableBlocks);
+            Assert.AreEqual(5_200_000_000_000_000uL, actualProtocolSettings.InitialGasDistribution);
+            Assert.AreEqual(1, actualProtocolSettings.ValidatorsCount);
+
+            Assert.IsNotNull(actualProtocolSettings.Hardforks);
+            Assert.AreEqual(666u, actualProtocolSettings.Hardforks[Hardfork.HF_Aspidochelone]);
+
+            Assert.IsNotNull(actualProtocolSettings.StandbyCommittee);
+            Assert.AreEqual(ECPoint.Parse("03b209fd4f53a7170ea4444e0cb0a6bb6a53c2bd016926989cf85f9b0fba17a70c", ECCurve.SecP256r1), actualProtocolSettings.StandbyCommittee[0]);
+
+            Assert.IsNotNull(actualProtocolSettings.SeedList);
+            Assert.AreEqual("seed1.neo.org:10333", actualProtocolSettings.SeedList[0]);
         }
     }
 }
